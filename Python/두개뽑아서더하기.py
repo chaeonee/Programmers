caf6b57ee7a2e5da82e277912f7548761458c15e
chaeonee <31956177+chaeonee@users.noreply.github.com>
2021-03-16 15:40:18 +0900

    Create 두개뽑아서더하기.py

---
+++ Python/두개뽑아서더하기.py
@@ -0,0 +1,14 @@
+def solution(numbers):
+    answer = set()
+
+    numbers.sort()
+
+    n = len(numbers)
+    for i in range(n):
+        for j in range(i+1,n):
+            answer.add(numbers[i]+numbers[j])
+
+    answer = list(answer)
+    answer.sort()
+
+    return answer
